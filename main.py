bb0b558acdbcb7b4b26860c6cb3ade46c47227fa
bellinebos <belline.bos@gmail.com>
2025-01-08 05:40:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-def read_job_operations_from_file(file_path):
+def read_job_operations_from_file(file_name):
     job_operations = []
-    with open(file_path, 'r') as file:
+    with open(file_name, 'r') as file:
         lines = file.readlines()
-        current_job_operations = []
-        for line in lines:
-            if line.strip():  # Skip empty lines
-                operations = list(map(int, line.strip().split()))
-                current_job_operations.append(operations)
-            else:
-                job_operations.append(current_job_operations)
-                current_job_operations = []
-        if current_job_operations:
-            job_operations.append(current_job_operations)
+        
+        num_jobs = len(lines) // 2  # Since we have two parts for each job
+        for i in range(num_jobs):
+            line1 = list(map(int, lines[i * 2].split()))
+            line2 = list(map(int, lines[i * 2 + 1].split()))
+            
+            job_operations.append(list(zip(line1[::2], line1[1::2])))  # Machine, Processing time pairs for the first line
+            job_operations.append(list(zip(line2[::2], line2[1::2])))  # Machine, Processing time pairs for the second line
+            
     return job_operations
 
 # Read job operations from a text file
