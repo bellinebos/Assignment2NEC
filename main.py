b9d1ffb15224c51fd3022f2b781a95d50023d7f6
bellinebos <belline.bos@gmail.com>
2025-01-08 05:43:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 def read_job_operations_from_file(file_name):
     job_operations = []
     
-    try:
-        with open(file_name, 'r') as file:
-            lines = file.readlines()
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
 
-            for line in lines:
-                operations = []
-                values = list(map(int, line.split()))  # Read all integers in the line
-                # Group values in pairs (machine, processing_time)
-                for i in range(0, len(values), 2):
-                    operations.append((values[i], values[i+1]))  # (machine, time)
-                job_operations.append(operations)
+        for line in lines:
+            operations = []
+            values = list(map(int, line.split()))  # Read all integers in the line
+            # Group values in pairs (machine, processing_time)
+            for i in range(0, len(values), 2):
+                operations.append((values[i], values[i+1]))  # (machine, time)
+            job_operations.append(operations)
 
-    except Exception as e:
-        print(f"Error reading file: {e}")
-        return []
-
-    print(f"Job operations loaded: {len(job_operations)} jobs")  # Debug print to check
     return job_operations
 
 # Read job operations from a text file
